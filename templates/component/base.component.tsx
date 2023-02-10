3ea33df0fc51b0a334b664a8977ea996185173cc
Karol Binkowski <starystachu9@gmail.com>
2023-02-10 15:32:40 +0100

    build: add component templates

---
+++ templates/component/base.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {} from 'react-native';
+
+import styles from './base.styles';
+
+export interface BaseProps {
+  text: string;
+}
+
+export function Base(props: BaseProps) {
+  const { text } = props;
+
+  return <></>;
+}
+
+export default Base;
